b9f2671cc7a3d6d94dc2e57d9d856918f09483a3
Diogo Gonçalves <2211879@my.ipleiria.pt>
2023-12-08 23:19:56 +0000

    Create Layouts (Dados Utente + Menu + Diagnóstico)
    Dados Utente + Menu + Diagnóstico

---
+++ app/src/main/java/com/example/dentalcare/DiagnosticoActivity.java
@@ -0,0 +1,14 @@
+package com.example.dentalcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiagnosticoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diagnostico);
+    }
+}
