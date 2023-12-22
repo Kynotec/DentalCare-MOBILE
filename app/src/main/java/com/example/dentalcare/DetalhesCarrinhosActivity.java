f481fb4862a50b46ec1883d1896eb0d2fcc73dba
Luis Ferreira <2211882@my.ipleiria.pt>
2023-12-22 17:52:20 +0000

    Layout's Produtos | Carrinho

---
+++ app/src/main/java/com/example/dentalcare/DetalhesCarrinhosActivity.java
@@ -0,0 +1,14 @@
+package com.example.dentalcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalhesCarrinhosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_carrinhos);
+    }
+}
---
+++ app/src/main/java/com/example/dentalcare/DetalhesProdutosActivity.java
@@ -0,0 +1,14 @@
+package com.example.dentalcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalhesProdutosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_produtos);
+    }
+}
